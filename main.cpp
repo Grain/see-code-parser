8ae80864b37b14630fdbac4959a929a3936d156e
Paul He <paulhe55555@gmail.com>
2014-03-28 21:46:50 -0400

    Added reading c source file

--- main.cpp
+++ main.cpp
@@ -1,51 +1,75 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cctype>
+#include "function.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
+vector<string> readFile(char * filename)
 {
     ifstream input;
 
-    if (argc > 1)
-    {
-        input.open(argv[1]);
-        if (input.is_open() == false)
-        {
-            //error opening
-            return 0;
-        }
-        else
-        {
-            string line;
-            int next;
+    input.open(filename);
+    vector<string> file;
 
-            while ((next = input.peek()) != EOF)
+    if (input.is_open() == false)
+    {
+        //error opening
+    }
+    else    //read the entire file
+    {
+        string line;
+
+        while (getline(input, line) != NULL)
+        {
+            if (line.size() > 0 && line[0] == '#')
             {
-                if (next == '#')
+                //ignore line
+            }
+            else if (line.size() == 0)
+            {
+                //ignore
+            }
+            else
+            {
+                for (unsigned int i = 0; i < line.size(); ++i)  //if the entire line is whitespace don't put in vector
                 {
-                    //ignore line
-                    getline(input, line);
-                }
-                else if (next == '\n')
-                {
-                    //ignore line
-                    getline(input, line);
-                }
-                else
-                {
-                    getline(input, line);
-                    cout << line << endl;
+                    if (isspace(line.at(i)) == false)
+                    {
+                        file.push_back(line);
+                        break;
+                    }
                 }
             }
+        }
+
+        for (unsigned int i = 0; i < file.size(); ++i)
+        {
+            cout << file.at(i) << endl;
+        }
+    }
+
+    return file;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc > 1)
+    {
+        vector<string> fileContents = readFile(argv[1]);
+        if (fileContents.empty() == true)
+        {
+            cout << "Error opening file" << endl;
+            return 0;
         }
     }
     else
     {
         //error: no file provided
+        cout << "Must provide a c source file as an argument" << endl;
         return 0;
     }
 
-//    cout << "Hello world!" << endl;
-//    return 0;
+
 }
---
+++ src/function.cpp
@@ -0,0 +1,11 @@
+#include "function.h"
+
+Function::Function()
+{
+    //ctor
+}
+
+Function::~Function()
+{
+    //dtor
+}
