eb9849254f27bd9dfeae04fce9ad1c707f4a208e
Paul He <paulhe55555@gmail.com>
2014-03-28 21:00:47 -0400

    starting on file input

--- main.cpp
+++ main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    ifstream input;
+
+    if (argc > 1)
+    {
+        input.open(argv[1]);
+        if (input.is_open() == false)
+        {
+            //error opening
+            return 0;
+        }
+        else
+        {
+            string line;
+            int next;
+
+            while ((next = input.peek()) != EOF)
+            {
+                if (next == '#')
+                {
+                    //ignore line
+                    getline(input, line);
+                }
+                else if (next == '\n')
+                {
+                    //ignore line
+                    getline(input, line);
+                }
+                else
+                {
+                    getline(input, line);
+                    cout << line << endl;
+                }
+            }
+        }
+    }
+    else
+    {
+        //error: no file provided
+        return 0;
+    }
+
+//    cout << "Hello world!" << endl;
+//    return 0;
 }
---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 1;
+	
+	if (i == 1)
+	{
+		printf("Test\n");
+	}
+	
+	return 0;
+}
